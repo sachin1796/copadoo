3d635232a16c59de87dc81a7d295527c89a5532e
sachin_17 <sachin.vish17@gmail.com>
2024-10-16 14:31:28 +0530

    first commit

---
+++ src/Components/HomePage/Header.js
@@ -0,0 +1,16 @@
+// Header.js
+import React from 'react';
+
+
+
+const Header = () => {
+  return (
+    <div className='flex justify-end space-x-3 h-10  bg-slate-500'>
+        <h1 className='my-2 font-bold text-white'>Community Login</h1>
+        <h1 className='my-2 font-bold text-white'>DevOps Exchange</h1>
+        <h1 className='my-2 font-bold text-white mr-2'>Partners</h1>
+    </div>
+      );
+};
+
+export default Header;
---
+++ src/Components/HomePage/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import {Link} from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <div>
+        <div className='h-16  bg-slate-500'>
+
+            <button>
+                Book a Demo
+            </button>
+            
+         
+       
+    </div>
+
+    </div>
+  )
+}
+
+export default NavBar
